a8194916c1e95aaba286423f30a774f86ee92a06
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-09-11 12:29:03 +0200

    Use IntersectionObserver when it's available

--- src/hoc/trackWindowScroll.js
+++ src/hoc/trackWindowScroll.js
@@ -2,53 +2,64 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import debounce from 'lodash.debounce';
 import throttle from 'lodash.throttle';
+import isIntersectionObserverAvailable from '../utils/intersection-observer';
 
 const trackWindowScroll = (BaseComponent) => {
   class ScrollAwareComponent extends React.Component {
     constructor(props) {
       super(props);
 
-      const onChangeScroll = this.onChangeScroll.bind(this);
+      if (!isIntersectionObserverAvailable()) {
+        const onChangeScroll = this.onChangeScroll.bind(this);
 
-      if (props.delayMethod === 'debounce') {
-        this.delayedScroll = debounce(onChangeScroll, props.delayTime);
-      } else if (props.delayMethod === 'throttle') {
-        this.delayedScroll = throttle(onChangeScroll, props.delayTime);
+        if (props.delayMethod === 'debounce') {
+          this.delayedScroll = debounce(onChangeScroll, props.delayTime);
+        } else if (props.delayMethod === 'throttle') {
+          this.delayedScroll = throttle(onChangeScroll, props.delayTime);
+        }
+
+        this.state = {
+          scrollPosition: {
+            x: window.scrollX || window.pageXOffset,
+            y: window.scrollY || window.pageYOffset,
+          },
+        };
       }
-
-      this.state = {
-        scrollPosition: {
-          x: window.scrollX || window.pageXOffset,
-          y: window.scrollY || window.pageYOffset,
-        },
-      };
     }
 
     componentDidMount() {
-      window.addEventListener('scroll', this.delayedScroll);
-      window.addEventListener('resize', this.delayedScroll);
+      if (!isIntersectionObserverAvailable()) {
+        window.addEventListener('scroll', this.delayedScroll);
+        window.addEventListener('resize', this.delayedScroll);
+      }
     }
 
     componentWillUnmount() {
-      window.removeEventListener('scroll', this.delayedScroll);
-      window.removeEventListener('resize', this.delayedScroll);
+      if (!isIntersectionObserverAvailable()) {
+        window.removeEventListener('scroll', this.delayedScroll);
+        window.removeEventListener('resize', this.delayedScroll);
+      }
     }
 
     onChangeScroll() {
-      this.setState({
-        scrollPosition: {
-          x: window.scrollX || window.pageXOffset,
-          y: window.scrollY || window.pageYOffset,
-        },
-      });
+      if (!isIntersectionObserverAvailable()) {
+        this.setState({
+          scrollPosition: {
+            x: window.scrollX || window.pageXOffset,
+            y: window.scrollY || window.pageYOffset,
+          },
+        });
+      }
     }
 
     render() {
       const { delayMethod, delayTime, ...props } = this.props;
+      const scrollPosition = isIntersectionObserverAvailable() ?
+        null : this.state.scrollPosition;
 
       return (
         <BaseComponent
-          scrollPosition={this.state.scrollPosition}
+          scrollPosition={scrollPosition}
           {...props} />
       );
     }
